459b98480bb290f3c0498c18e104ac0daf398ed1
Rob Brentnall <rob.brentnall@gmail.com>
2017-02-06 12:23:36 +0000

    fixed app.rb

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,16 @@
+ENV['RACK_ENV'] ||= 'development'
+
 require 'sinatra/base'
+require 'sinatra/flash'
+require 'sinatra/partial'
+require 'slim'
+require 'slim/include'
+# require_relative 'data_mapper_setup'
+# require_relative 'server'
+# require_relative 'helpers'
+# require_relative 'controllers/users'
+# require_relative 'controllers/sessions'
+# require_relative 'controllers/peeps'
+# require_relative 'controllers/comments'
 
-class Makersbnb < Sinatra::Base
-  get '/' do
-    'Hello Makersbnb!'
-  end
-
-  # start the server if ruby file executed directly
-  run! if app_file == $0
-end
+# include Helpers
