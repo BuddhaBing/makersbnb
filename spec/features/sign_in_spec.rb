e2106ac3ac4ace69ce4ee8ae34773b4bb554b20b
James Turner <mail.jt@gmail.com>
2017-02-08 10:14:54 +0000

    easiest ticket ever

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,5 +1,5 @@
 feature 'Sign in' do
-  xscenario 'I cannot sign in if I have not signed up' do #TODO all tests pending flash
+  scenario 'I cannot sign in if I have not signed up' do #TODO all tests pending flash
     sign_in
     expect(current_path).to eq '/sessions/new'
     expect(page).to have_content('The email or password entered is incorrect')
@@ -8,18 +8,15 @@ feature 'Sign in' do
     sign_up
     sign_out
     sign_in
-
-
-  #  expect(current_path).to eq '/users'
     expect(page).to have_content('Welcome, test')
   end
-  xscenario 'I cannot sign in if I enter my email incorrectly' do
+  scenario 'I cannot sign in if I enter my email incorrectly' do
     sign_up
     sign_in(email: 'something@something.com')
     expect(current_path).to eq '/sessions/new'
     expect(page).to have_content('The email or password entered is incorrect')
   end
-  xscenario 'I cannot sign in if I enter my password incorrectly' do
+  scenario 'I cannot sign in if I enter my password incorrectly' do
     sign_up
     sign_in(password: 'something')
     expect(current_path).to eq '/sessions/new'
