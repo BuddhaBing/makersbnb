7f6a859f137688d79c8304d6f8dac924898f11a8
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-02-08 12:12:34 +0000

    closes#33-pending_bookings

--- spec/helpers/bookings_helper.rb
+++ spec/helpers/bookings_helper.rb
@@ -3,3 +3,34 @@ def make_booking
   # select a calendar date
   #click_button("Request to book")
 end
+
+def bookings_made
+  #User 1 signs up and lists a room
+  sign_up
+  list_room(name: "Trump Towers")
+  visit('/')
+  sign_out
+  #User 2 signs up and books the room
+  sign_up(username: "test02",
+          email: "test02@test.com")
+  make_booking
+  click_link("My Bookings")
+
+end
+
+def bookings_received
+  #User 1 signs up and lists a room
+  sign_up
+  list_room(name: "The Penthouse")
+  visit('/')
+  sign_out
+  #User 2 signs up and books the room
+  sign_up(username: "test02",
+          email: "test02@test.com")
+  make_booking
+  sign_out
+  #User 1 signs in and checks bookings page
+  sign_in
+  click_link("My Bookings")
+
+end
