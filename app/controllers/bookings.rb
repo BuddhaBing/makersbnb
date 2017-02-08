879432103575bebd5a99213daf387b62594b3c4e
Benjamin Carson <benjohncarson@gmail.com>
2017-02-08 16:32:12 +0000

    small test refactor

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -3,4 +3,9 @@ class Makersbnb < Sinatra::Base
   get '/bookings' do
     slim :'bookings/index'
   end
+  
+  post '/bookings' do
+    #TODO Input database stuff once date ranges implemented
+    redirect '/bookings'
+  end
 end
--- spec/features/book_room_spec.rb
+++ spec/features/book_room_spec.rb
@@ -2,13 +2,11 @@ feature "Book a room" do
   #waiting on being able to make bookings
   scenario "Select a room from list of rooms" do
     select_room
-    expect(page).to have_content("Trump Towers")
+    expect(page).to have_content Room.first.name
   end
 
   scenario "request dates and room" do
     book_room
-    expect(page).to have content (#TODO once dates in database implemented)
+    expect(page).to have_content() #TODO once dates in database implemented
   end
-
-
 end
