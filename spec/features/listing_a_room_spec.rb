d05ed1505f18621459588bf8dad4b3c4017984ec
Rob Brentnall <rob.brentnall@gmail.com>
2017-02-08 12:36:20 +0000

    rooms index

--- spec/features/listing_a_room_spec.rb
+++ spec/features/listing_a_room_spec.rb
@@ -1,11 +1,11 @@
 feature 'Listing a room' do
   scenario 'As a landlord, I can list a new room when signed in' do
-    sign_up #TODO: uncomment this when signup works
-    # expect(page).to have_button 'New Room' TODO: uncomment this when signup works and welcome.slim has this button
+    sign_up
+    expect(page).to have_link 'List a Room'
     expect{list_room}.to change(Room, :count).by(1)
-  #expect(page).to have_content 'Room Listed'
+    expect(page).to have_content 'Room Listed'
   end
   scenario 'I cannot list a new room when not signed in' do
-    expect(page).not_to have_button 'New Room'
+    expect(page).not_to have_button 'List a Room'
   end
 end
