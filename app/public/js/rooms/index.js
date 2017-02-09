cd71b81e6a75564bd72889839d52bcb47c2a6646
James Turner <mail.jt@gmail.com>
2017-02-09 16:25:22 +0000

    36-edit_room

---
+++ app/public/js/rooms/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $("#edit_room").click(function(){
+    $(".edit_room_form").show(1500);
+    $(".info_room").hide(1500);
+    $("#edit_room").hide(1500);
+  });
+});
