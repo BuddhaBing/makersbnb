ff188b776bddfc3b8782d07fda1d0c1a683a8132
Rob Brentnall <rob.brentnall@gmail.com>
2017-02-09 16:32:04 +0000

    refactor

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -7,11 +7,11 @@ class Makersbnb < Sinatra::Base
   end
 
   post '/bookings' do
-    date = DateRange.first_or_create(start_date: params[:from],
-                                     end_date: params[:to])
-    booking = Booking.create(user_id: session[:user_id],
-                             room_id: params[:room_id],
-                             date_range_id: date.id)
+    DateRange.first_or_create(start_date: params[:from],
+                              end_date: params[:to])
+    Booking.create(user_id: session[:user_id],
+                   room_id: params[:room_id],
+                   date_range_id: date.id)
     redirect '/bookings'
   end
 
