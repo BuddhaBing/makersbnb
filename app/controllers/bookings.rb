18920c0cfc08498351824138e4c58f2a3bd78e52
PeteGH <peter@grant-hay.eu>
2017-02-09 12:17:03 +0100

    adds view requested bookings

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -1,9 +1,10 @@
 class Makersbnb < Sinatra::Base
 
   get '/bookings' do
+    @bookings = Booking.all(id: current_user.id)
     slim :'bookings/index'
   end
-  
+
   post '/bookings' do
     #TODO Input database stuff once date ranges implemented
     redirect '/bookings'
