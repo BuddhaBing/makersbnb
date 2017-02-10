3113a00d9e0b51886e331609b8328c9efe79aea6
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-02-10 10:36:42 +0000

    feature test for comfirming bookings done

---
+++ spec/features/approve_bookings_spec.rb
@@ -0,0 +1,14 @@
+feature "Confirm a booking " do
+  scenario "User can confirm a booking made to their room" do
+    create_users_and_room
+    book_room
+    sign_out
+    sign_in
+    click_link ('My Bookings')
+    click_button ('Accept')
+     bookings_received = find_by_id("bookings_received")
+     expect(bookings_received).to_not have_content ('pending')
+    expect(bookings_received).to have_content ('confirmed')
+
+  end
+end
